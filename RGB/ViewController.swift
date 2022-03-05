b156570fb93fcc66ee171e7f3009bc8c5cb9992d
Pomortsev Andrey <avpomortsev@icloud.com>
2022-03-05 20:01:41 +0300

    coding and release :)

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -10,15 +10,61 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var colorView: UIView!
-    @IBOutlet var redValueLabel: UILabel!
-    @IBOutlet var greenValueLabel: UILabel!
-    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         colorView.layer.cornerRadius = 10
+        
+        setColorView()
+        setValueLabel()
+        
+    }
+
+    private func setColorView() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1.0)
         
     }
     
+    private func setValueLabel() {
+        redLabel.text = getValue(from: redSlider)
+        greenLabel.text = getValue(from: greenSlider)
+        blueLabel.text = getValue(from: blueSlider)
+    }
+    
+    private func getValue(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+
+    @IBAction func changeRGBView(_ sender: UISlider) {
+        
+        switch sender.tag {
+        case 0:
+            redLabel.text = getValue(from: sender)
+        case 1:
+            greenLabel.text = getValue(from: sender)
+        case 2:
+            blueLabel.text = getValue(from: sender)
+        default:
+            break
+        }
+       setColorView()
+    }
+    
 }
+    
+   
+
+
 
