bf7234640a275864899de1c4290cf1b5969ad4d4
Pomortsev Andrey <avpomortsev@icloud.com>
2022-03-05 12:20:02 +0300

    Arrangement UI elements.

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        
     }
-
-
+    
 }
 
